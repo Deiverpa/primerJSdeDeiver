599901432d23e6d4d742e6581b469251eb4f586d
Deiverpa <deipa280@hotmail.com>
2022-04-24 19:16:32 -0500

    mergedbranch&main

--- js/scs.js
+++ js/scs.js
@@ -1,19 +1,25 @@
-const tasaDeCambioUSDBTC = 40139
-const tasaDeCambioBTCUSD = 0.000025
+// const tasaDeCambioUSDBTC = 40139
+// const tasaDeCambioBTCUSD = 0.000025
 
-let mercado = prompt("BTC o USD");
-if ((mercado == "USD")) {
-let cantidadUSD = prompt("Ingrese Cantidad USD")
-const cryptoExchange = (tasaDeCambioUSDBTC, cantidadUSD) =>
-tasaDeCambioUSDBTC * cantidadUSD
-alert("USD a BTC =" + " " +(cryptoExchange(tasaDeCambioBTCUSD, cantidadUSD,)));
-} else if((mercado == "BTC")) {
-    let cantidadBTC = prompt("Ingrese Cantidad BTC")
-    const cryptoExchange = (tasaDeCambioBTCUSD, cantidadBTC) =>
-    tasaDeCambioUSDBTC * cantidadBTC
-    alert("BTC a USD =" + " " + (cryptoExchange(tasaDeCambioBTCUSD, cantidadBTC,)));
-    } else {
-        (prompt("recargue la pagina y digite nuevamente la moneda"))
+// let mercado = prompt("BTC o USD");
+// if ((mercado == "USD")) {
+// let cantidadUSD = prompt("Ingrese Cantidad USD")
+// const cryptoExchange = (tasaDeCambioUSDBTC, cantidadUSD) =>
+// tasaDeCambioUSDBTC * cantidadUSD
+// alert("USD a BTC =" + " " +(cryptoExchange(tasaDeCambioBTCUSD, cantidadUSD,)));
+// } else if((mercado == "BTC")) {
+//     let cantidadBTC = prompt("Ingrese Cantidad BTC")
+//     const cryptoExchange = (tasaDeCambioBTCUSD, cantidadBTC) =>
+//     tasaDeCambioUSDBTC * cantidadBTC
+//     alert("BTC a USD =" + " " + (cryptoExchange(tasaDeCambioBTCUSD, cantidadBTC,)));
+//     } else {
+//         (prompt("recargue la pagina y digite nuevamente la moneda"))
+//     }
+
+class asset{
+    constructor(nombre, cantidad){
+        this.nombre  = nombre;
+        this.cantidad  = cantidad;
     }
     compra(){
         console.log("Tienes: " + this.cantidad + " " + this.nombre );
