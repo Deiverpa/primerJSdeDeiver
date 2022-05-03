5a08bafe5a670a18672deebc27baa07a7e4a1b66
Deiverpa <deipa280@hotmail.com>
2022-05-03 05:06:27 -0500

    stylesaddition

---
+++ js/api.js
@@ -0,0 +1,18 @@
+// Libreria https://github.com/infodp/crypto_cotizacion_vanilla_js
+// Expliación https://www.youtube.com/watch?v=IAOHydmDrmQ
+
+let endpoint = 'https://api.binance.com/api/v3/ticker/price'
+fetch(endpoint)
+    .then( respuesta => respuesta.json() )
+    .then( datos => mostrarData(datos))
+    .catch( e => console.log(e))
+
+
+const mostrarData = (data)=>{
+    //console.log(data)
+    let body = ''
+    for (let i=0; i < data.length; i++) {
+        body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+    }
+    document.getElementById('data').innerHTML = body
+}    
