c21ab6d47301496ec6170edd6c8324ed99bb348d
Deiverpa <deipa280@hotmail.com>
2022-05-17 23:57:25 -0500

    ImplementaciónCompra

--- js/scs.js
+++ js/scs.js
@@ -7,6 +7,7 @@ class currency {
     }
 }
 
+let walletUser = []
 /* Creating a prompt to enter a username and then it is saving it in the session storage. */
 let usernameID = "user";
 
@@ -19,43 +20,48 @@ sessionStorage.setItem("saveusername",usernameID)
 }
 
 /* Creating a new currency object with the name USD, value 1 and quantity 5000. */
+let currencyInicial = new currency
 function nuevousuario() {
-    const currencyUSD = new currency("USD", 1, 5000);
-    console.log(currencyUSD);
+    let currencyInicial = new currency("USD", 1, 5000);
+    walletUser.push(currencyInicial);
+    console.log(currencyInicial)
+    console.log(walletUser)
 }
 
-
-function compra() {
-    compramoneda = currencyUSD.quantity + compraUSD;
-}
-
-const currencyUSD = 0
 const startTrading = document.getElementById("getStarted")
 startTrading.onclick = () =>{
     nuevousuario();
 }
 
-
-let compraUSD = 0
-let buyUSD = document.getElementById("buyButtonUSD")
-buyUSD.onclick = () =>{ if(usernameID == ""){
-    alert ("please sign in")} else{
-    let compraUSD = prompt("How many USD do you want to buy?")
-    if(compraUSD <= 0){
-        alert ("please buy more than 0 units")
-        }
-        else {
-            compra();
-            alert ("you bought " + compraUSD + " USD Units");
-            let parrafo = document.createElement("p");
-            parrafo.innerHTML = "<strong><h2>Your Wallet has been increased on </h2>" + compraUSD + " USD</strong>"; 
-            document.body.append(parrafo);
-            currencyUSD.quantity = compraUSD + currencyUSD.quantity;
-            console.log(currencyUSD)         
-        }
-    }       
+let purchaseUSD = new currency
+function purchasesUSD() {
+    let purchaseUSD = new currency("USD", 1 , Number(prompt("how many USD do you want to buy")));
+    walletUser.push(purchaseUSD);
+    console.log(purchaseUSD)
+    console.log(walletUser)
 }
 
+let buyUSD = document.getElementById("buyButtonUSD")
+buyUSD.onclick = () =>{ purchasesUSD();     
+}
+
+// let buyUSD = document.getElementById("buyButtonUSD")
+// buyUSD.onclick = () =>{ if(usernameID == ""){
+//     alert ("please sign in")} else{
+//     // let compraUSD = prompt("How many USD do you want to buy?")
+//     // if(compraUSD <= 0){
+//     //     alert ("please buy more than 0 units")
+//     //     }
+//     //     else {
+//             alert ("you bought " + compraUSD + " USD Units");
+//             let parrafo = document.createElement("p");
+//             parrafo.innerHTML = "<strong><h2>Your Wallet has been increased on </h2>" + compraUSD + " USD</strong>"; 
+//             document.body.append(parrafo);
+//             // currency1.quantity = currency1.quantity + compraUSD;
+//             console.log(currencyInicial)       
+//         }
+//     // }       
+// }
 
 let buyBTC = document.getElementById("buyButtonBTC")
 buyBTC.onclick = () =>{ if(usernameID == ""){
