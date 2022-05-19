7227125460479342cdd9bc451a322945e6ae1cd8
Deiverpa <deipa280@hotmail.com>
2022-05-19 14:17:20 -0500

    operadoravanzado

--- js/scs.js
+++ js/scs.js
@@ -9,7 +9,7 @@ class currency {
 
 let walletUser = []
 /* Creating a prompt to enter a username and then it is saving it in the session storage. */
-let usernameID = "user";
+let usernameID = "";
 
 const signIn = document.getElementById("sign-in-up")
 signIn.onclick = () =>{
@@ -36,77 +36,29 @@ startTrading.onclick = () =>{
 let purchaseUSD = new currency
 function purchasesUSD() {
     let purchaseUSD = new currency("USD", 1 , Number(prompt("how many USD do you want to buy")));
-    walletUser.push(purchaseUSD);
+    walletUser.unshift(purchaseUSD);
+    alert ("you bought " + purchaseUSD + " USD Units");
+    let parrafo = document.createElement("p");
+    parrafo.innerHTML = "<strong><h2>Your Wallet has been increased on </h2>" + purchaseUSD + " USD</strong>"; 
+    document.body.append(parrafo);
+    console.log(currencyInicial)
     console.log(purchaseUSD)
     console.log(walletUser)
 }
 
 let buyUSD = document.getElementById("buyButtonUSD")
-buyUSD.onclick = () =>{ purchasesUSD();     
+buyUSD.onclick = () =>{usernameID == "" ? alert ("please sign in") : purchasesUSD();     
 }
 
-// let buyUSD = document.getElementById("buyButtonUSD")
-// buyUSD.onclick = () =>{ if(usernameID == ""){
-//     alert ("please sign in")} else{
-//     // let compraUSD = prompt("How many USD do you want to buy?")
-//     // if(compraUSD <= 0){
-//     //     alert ("please buy more than 0 units")
-//     //     }
-//     //     else {
-//             alert ("you bought " + compraUSD + " USD Units");
-//             let parrafo = document.createElement("p");
-//             parrafo.innerHTML = "<strong><h2>Your Wallet has been increased on </h2>" + compraUSD + " USD</strong>"; 
-//             document.body.append(parrafo);
-//             // currency1.quantity = currency1.quantity + compraUSD;
-//             console.log(currencyInicial)       
-//         }
-//     // }       
-// }
-
-let buyBTC = document.getElementById("buyButtonBTC")
-buyBTC.onclick = () =>{ if(usernameID == ""){
-    alert ("please sign in")} else{
-    let compraBTC = prompt("How many BTC do you want to buy?")
-    if(compraBTC <=0){
-        alert ("please buy more than 0 units")
-        }
-        else {
-            alert ("you bought " + compraBTC + " BTC Units");
-            let parrafo = document.createElement("p");
-            parrafo.innerHTML = "<strong><h2>Your Wallet has been increased on </h2>" + compraBTC + " BTC</strong>"; 
-            document.body.append(parrafo);           
-        }
-    }           
+let sellsUSD = new currency
+function sellesUSD() {
+    let sellsUSD = new currency("USD", 1 , Number(prompt("how many USD do you want to sell")));
+    walletUser.shift(sellsUSD);
+    console.log(sellsUSD)
+    console.log(walletUser)
 }
 
 let sellUSD = document.getElementById("sellButtonUSD")
-sellUSD.onclick = () =>{if(usernameID == ""){
-    alert ("please sign in")} else{
-    let ventaUSD = prompt("How many USD do you want to sell?")
-    if(ventaUSD <=0){
-        alert ("please sell more than 0 units")
-        }
-        else {
-            alert ("you sold " + ventaUSD + " USD Units")
-            let parrafo = document.createElement("p");
-            parrafo.innerHTML = "<strong><h2>Your Wallet has been reduced in </h2>" + ventaUSD + " USD</strong>"; 
-            document.body.append(parrafo);
-        }
-    }    
+sellUSD.onclick = () =>{ sellesUSD();     
 }
 
-let sellBTC = document.getElementById("sellButtonBTC")
-sellBTC.onclick = () =>{if(usernameID == ""){
-    alert ("please sign in")} else{
-    let ventaBTC = prompt("How many BTC do you want to sell?")
-    if(ventaBTC <=0){
-        alert ("please sell more than 0 units")
-        }
-        else {
-            alert ("you sold " + ventaBTC + " BTC Units");
-            let parrafo = document.createElement("p");
-            parrafo.innerHTML = "<strong><h2>Your Wallet has been reduced in </h2>" + ventaBTC + " BTC</strong>"; 
-            document.body.append(parrafo);           
-        }
-    }
-}
