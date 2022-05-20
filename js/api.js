4ab7233f987a6381c5a1533969289564e0699f6d
Deiverpa <deipa280@hotmail.com>
2022-05-19 22:56:17 -0500

    inclusiónLibreriayAPI

--- js/api.js
+++ js/api.js
@@ -9,10 +9,10 @@ fetch(endpoint)
 
 
 const mostrarData = (data)=>{
-    //console.log(data)
+    console.log(data)
     let body = ''
     for (let i=0; i < data.length; i++) {
         body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
     }
-    document.getElementById('data').innerHTML = body
+    document.getElementById('tdata').innerHTML = body
 }    
